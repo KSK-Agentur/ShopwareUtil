68db1db9d7ffa13715a7c2ac98b87087b8579c53
Joshua Behrens <behrens@ksk-agentur.de>
2018-09-27 00:09:42 +0200

    Added deleteData method

--- src/Components/Updater/AlterDatabaseData.php
+++ src/Components/Updater/AlterDatabaseData.php
@@ -14,7 +14,7 @@ trait AlterDatabaseData
     use HasDbalConnection;
 
     /**
-     * @param $table
+     * @param string   $table
      * @param callable $transformer
      *
      * @throws DBALException
@@ -30,7 +30,7 @@ trait AlterDatabaseData
         }
 
         $primaryKeys = $tableDetails->getPrimaryKeyColumns();
-        $queryBuilder = $this->getAlterDatabaseDataConnection()->createQueryBuilder();
+        $queryBuilder = $this->getDbalConnection()->createQueryBuilder();
         $queryBuilder->from($table)->select(['*']);
 
         foreach ($queryBuilder->execute()->fetchAll(\PDO::FETCH_ASSOC) as $row) {
@@ -39,6 +39,34 @@ trait AlterDatabaseData
                 $changes = array_diff_assoc($newData, $row);
                 $primerayKeyValues = array_intersect_key($row, array_fill_keys($primaryKeys, true));
                 $connection->update($table, $changes, $primerayKeyValues);
+            }
+        }
+    }
+
+    /**
+     * @param string   $table
+     * @param callable $predicate
+     *
+     * @throws DBALException
+     * @throws LogicException
+     */
+    public function deleteData($table, callable $predicate)
+    {
+        $connection = $this->getDbalConnection();
+        $tableDetails = $connection->getSchemaManager()->listTableDetails($table);
+
+        if (!$tableDetails->hasPrimaryKey()) {
+            throw new LogicException('Table ' . $table . ' has no primary key');
+        }
+
+        $primaryKeys = $tableDetails->getPrimaryKeyColumns();
+        $queryBuilder = $this->getDbalConnection()->createQueryBuilder();
+        $queryBuilder->from($table)->select(['*']);
+
+        foreach ($queryBuilder->execute()->fetchAll(\PDO::FETCH_ASSOC) as $row) {
+            if (call_user_func($predicate, $row)) {
+                $primerayKeyValues = array_intersect_key($row, array_fill_keys($primaryKeys, true));
+                $connection->delete($table, $primerayKeyValues);
             }
         }
     }
